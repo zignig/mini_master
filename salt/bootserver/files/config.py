f18834231684f360a9df66d86425c5b47c0dac96
Simon Kirkby <tigger@interthingy.com>
2014-03-30 15:44:12 +0800

    move the repo ref to github

--- salt/bootserver/files/config.py
+++ salt/bootserver/files/config.py
@@ -4,4 +4,4 @@ sqlurl = 'sqlite:////tmp/test.db'
 salt_master = '{{ salt['network.ip_addrs']()[0] }}'
 has_proxy = True
 proxy = "http://{{ salt['network.ip_addrs']()[0] }}:3142"
-salt_repo = "http://bl3dr.com/git/mini_master.git"
+salt_repo = "https://github.com/zignig/exarch.git"
