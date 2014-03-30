6c8e60f15ba421010b673a0d366426c76079ca58
Simon Kirkby <tigger@interthingy.com>
2014-03-31 06:46:47 +0800

    ask for root password

--- salt/bootserver/files/config.py
+++ salt/bootserver/files/config.py
@@ -5,3 +5,4 @@ salt_master = '{{ salt['network.ip_addrs']()[0] }}'
 has_proxy = True
 proxy = "http://{{ salt['network.ip_addrs']()[0] }}:3142"
 salt_repo = "https://github.com/zignig/exarch.git"
+password = None
