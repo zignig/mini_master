b7275b76476bc45059a81271943bf5bb0b74bf9c
zignig <tigger@interthingy.com>
2014-03-31 09:51:51 +0800

    Update config.py
    pointing to exarch for salt base , incorrect destination.

--- salt/bootserver/files/config.py
+++ salt/bootserver/files/config.py
@@ -4,6 +4,6 @@ sqlurl = 'sqlite:////tmp/test.db'
 salt_master = '{{ salt['network.ip_addrs']()[0] }}'
 has_proxy = True
 proxy = "http://{{ salt['network.ip_addrs']()[0] }}:3142"
-salt_repo = "https://github.com/zignig/exarch.git"
+salt_repo = "https://github.com/zignig/mini_master.git"
 menu = '/opt/bootserver/menu.yaml'
 password = None
