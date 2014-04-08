e4b802cbe8102a2b617f5d1950bee872e55c4b15
Simon Kirkby <tigger@interthingy.com>
2014-04-08 23:19:37 +0800

    changed path on default exarch

--- salt/bootserver/files/config.py
+++ salt/bootserver/files/config.py
@@ -5,5 +5,5 @@ salt_master = '{{ salt['network.ip_addrs']()[0] }}'
 has_proxy = True
 proxy = "http://{{ salt['network.ip_addrs']()[0] }}:3142"
 salt_repo = "https://github.com/zignig/exarch.git"
-menu = '/opt/bootserver/menu.yaml'
+menu = '/opt/bootserver/config/menu.yaml'
 password = None
