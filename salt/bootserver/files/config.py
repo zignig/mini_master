7c2cca0e7280b83823df22d4ded8a7d0013e1cc4
Simon Kirkby <tigger@interthingy.com>
2014-04-11 22:31:37 +0800

    fixed for image path.
    Signed-off-by: Simon Kirkby <tigger@interthingy.com>

--- salt/bootserver/files/config.py
+++ salt/bootserver/files/config.py
@@ -6,4 +6,5 @@ has_proxy = True
 proxy = "http://{{ salt['network.ip_addrs']()[0] }}:3142"
 salt_repo = "https://github.com/zignig/mini_master.git"
 menu = '/opt/bootserver/config/menu.yaml'
+images = '/opt/bootserver/config/image_urls.txt'
 password = None
